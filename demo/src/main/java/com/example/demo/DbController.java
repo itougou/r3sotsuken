a77d8062403c472e2f566a531aeda07f15d87f2e
gogo <gogo@192.168.11.102>
2021-09-29 21:25:33 +0900

    最初のコミット

---
+++ demo/src/main/java/com/example/demo/DbController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/jdbc/sample")
+public class DbController {
+
+   @Autowired
+   JdbcTemplate jdbcTemplate;
+
+   @RequestMapping(path="/users", method=RequestMethod.GET)
+   public String index() {
+       List<Map<String,Object>> list;
+       list = jdbcTemplate.queryForList("select * from employee");
+       return list.toString();
+   }
+
+   @RequestMapping(path="/users/{id}", method=RequestMethod.GET)
+   public String read(@PathVariable String id) {
+       List<Map<String,Object>> list;
+       list = jdbcTemplate.queryForList("select * from employee where id = ?", id);
+       return list.toString();
+   }
+}
