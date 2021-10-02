58c9c6de3593a2721a580475fe0fce6684ecc47a
gogo <gogo@192.168.11.102>
2021-10-02 13:01:47 +0900

    10/2　18:38　マスターをコミット

--- demo/src/main/java/com/example/demo/DbController.java
+++ demo/src/main/java/com/example/demo/DbController.java
@@ -18,7 +18,7 @@ public class DbController {
    JdbcTemplate jdbcTemplate;
 
    @RequestMapping(path="/users", method=RequestMethod.GET)
-   public String index() {	//インデックス　メソッド　MAINブランチ
+   public String index() {	//インデックス　メソッド　★マスター★ブランチ
        List<Map<String,Object>> list;
        list = jdbcTemplate.queryForList("select * from employee");
        return list.toString();//戻り値返却
