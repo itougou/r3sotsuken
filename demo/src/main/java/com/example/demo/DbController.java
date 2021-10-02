f1981d397cfc63c73a1f4a12a7ce7e3ee89ace1b
gogo <gogo@192.168.11.102>
2021-10-02 18:47:06 +0900

    コミット　１０．２　１８：４６

--- demo/src/main/java/com/example/demo/DbController.java
+++ demo/src/main/java/com/example/demo/DbController.java
@@ -16,7 +16,8 @@ public class DbController {
 
    @Autowired
    JdbcTemplate jdbcTemplate;
-
+   
+ //　★マスター★ブランチ　２０２１．１０．２　１８：４６
    @RequestMapping(path="/users", method=RequestMethod.GET)
    public String index() {	//インデックス　メソッド　★マスター★ブランチ
        List<Map<String,Object>> list;
