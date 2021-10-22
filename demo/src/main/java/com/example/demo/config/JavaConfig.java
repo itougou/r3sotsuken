e99a3ea7dbd3e65e63b4b04715882dca01f6c473
gogo <gogo@192.168.11.102>
2021-10-23 08:13:26 +0900

    フィルタ　コメント修正

--- demo/src/main/java/com/example/demo/config/JavaConfig.java
+++ demo/src/main/java/com/example/demo/config/JavaConfig.java
@@ -8,51 +8,51 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.filter.MyFilter;
 import com.example.demo.filter.MyFilter2;
 
-//　Mapperのクラス（画面との受け渡し用のデータとエンティティの変換を行うクラス）
-//　使用するには、pow.xmlに以下の記述追加必要
-//<dependency>
-//<groupId>org.modelmapper.extensions</groupId>
-//<artifactId>modelmapper-spring</artifactId>
-//<version>0.7.3</version> 
-//</dependency>
-
 @Configuration
 public class JavaConfig {
 	@Autowired MyFilter myFilter;
 	@Autowired MyFilter2 myFilter2;
 	
-	//ModelMapper
+	/*　ModelMapper
+	　Mapperのクラス（画面との受け渡し用のデータとエンティティの変換を行うクラス）
+	　使用するには、pow.xmlに以下の記述追加必要
+	　<dependency>
+	　<groupId>org.modelmapper.extensions</groupId>
+	　<artifactId>modelmapper-spring</artifactId>
+	　<version>0.7.3</version> 
+	　</dependency>
+	*/
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 	
 	//フィルタの登録、フィルタパスの指定
-    @Bean
-    public FilterRegistrationBean myFilterBean() {	//スタッフ用フィルターの登録
-        // ServletContextInitializerBeansに格納される
-        //FilterRegistrationBean bean = new FilterRegistrationBean( new MyFilter() );
-        FilterRegistrationBean bean = new FilterRegistrationBean( myFilter );
-        // <url-pattern/>
-        //bean.addUrlPatterns("/customer/list");
-        bean.addUrlPatterns("/customer/list");	//顧客一覧
-        bean.addUrlPatterns("/customer/add");	//顧客追加
-        bean.addUrlPatterns("/customer/search");	//顧客検索
-        bean.addUrlPatterns("/purchase/list");	//顧客一覧
-        //bean.addUrlPatterns("/purchase/*");
-        // MyFilterがMyFilter2より先に呼ばれる
-        //bean.setOrder(1);
-        return bean;
-    }
-    @Bean
-    public FilterRegistrationBean myFilterBean2() {	//顧客用フィルターの登録
-        // ServletContextInitializerBeansに格納される
-        //FilterRegistrationBean bean = new FilterRegistrationBean( new MyFilter2() );
-        FilterRegistrationBean bean = new FilterRegistrationBean( myFilter2 );
-        // <url-pattern/>
-        bean.addUrlPatterns("/purchase/add");	//注文
-        // MyFilterがMyFilter2より先に呼ばれる
-        //bean.setOrder(1);
-        return bean;
-    }
+	@Bean
+	public FilterRegistrationBean myFilterBean() {	//スタッフ用フィルターの登録
+		// ServletContextInitializerBeansに格納される
+		//FilterRegistrationBean bean = new FilterRegistrationBean( new MyFilter() ); 自作クラスnewすると動作しない？代わりに@Autowierdを入れる
+		FilterRegistrationBean bean = new FilterRegistrationBean( myFilter );
+		// <url-pattern/>
+		//bean.addUrlPatterns("/customer/list");
+		bean.addUrlPatterns("/customer/list");	//顧客一覧
+		bean.addUrlPatterns("/customer/add");	//顧客追加
+		bean.addUrlPatterns("/customer/search");	//顧客検索
+		bean.addUrlPatterns("/purchase/list");	//顧客一覧
+		//bean.addUrlPatterns("/purchase/*");
+		// MyFilterがMyFilter2より先に呼ばれる
+		//bean.setOrder(1);
+		return bean;
+	}
+	@Bean
+	public FilterRegistrationBean myFilterBean2() {	//顧客用フィルターの登録
+		// ServletContextInitializerBeansに格納される
+		//FilterRegistrationBean bean = new FilterRegistrationBean( new MyFilter2() ); 自作クラスnewすると動作しない？代わりに@Autowierdを入れる
+		FilterRegistrationBean bean = new FilterRegistrationBean( myFilter2 );
+		// <url-pattern/>
+		bean.addUrlPatterns("/purchase/add");	//注文
+		// MyFilterがMyFilter2より先に呼ばれる
+		//bean.setOrder(1);
+		return bean;
+	}
 }
